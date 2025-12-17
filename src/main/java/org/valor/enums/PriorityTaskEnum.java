7c6d0d605dec42e3ad5486ddef5bec8e0cf2d840
V.Luapunov <bladll1999@mail.ru>
2025-12-17 21:54:27 +0300

    add main and add application.properties

---
+++ src/main/java/org/valor/enums/PriorityTaskEnum.java
@@ -0,0 +1,28 @@
+package org.valor.enums;
+
+public enum PriorityTaskEnum {
+    LOW(0),
+    MIDDLE(1),
+    HIGH(2);
+
+    private final Integer priority;
+
+    PriorityTaskEnum(Integer priority) {
+        this.priority = priority;
+    }
+
+    public PriorityTaskEnum getPriorityTask(Integer priority) {
+       switch (priority) {
+           case 1 -> {
+               return MIDDLE;
+           }
+           case 2 -> {
+               return HIGH;
+           }
+           default -> {
+               return LOW;
+           }
+       }
+    }
+
+}
